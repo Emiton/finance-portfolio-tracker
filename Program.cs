3d18ce4b936b633d2441c6481b0d9b3504010840
Emiton <emiton_alves@my.uri.edu>
2019-10-03 16:19:51 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace financial_scraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            WebScraper scraper = new WebScraper();
+            scraper.ScrapeData();
+        }
+    }
+}
---
+++ WebScraper.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace financial_scraper
+{
+    class WebScraper
+    {
+        private IWebDriver driver = new ChromeDriver();
+
+        public void ScrapeData()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com");
+            WebDriverWait wait = new WebDriverWait(driver, System.TimeSpan.FromSeconds(15));
+        }
+    }
+}
