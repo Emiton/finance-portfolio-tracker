56460f2c2fa1e70582f46ff9a2d93b4c8a3f5aeb
Emiton <emiton_alves@my.uri.edu>
2019-10-05 01:25:27 -0400

    Started processor

---
+++ StockProcessor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace financial_scraper
+{
+    // TODO: Make this class static
+    class StockProcessor
+    {
+        // Loop through table and create list of Stocks
+        // Return list
+    }
+}
