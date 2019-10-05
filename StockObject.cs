921e1a1789fff06bd80716d07fb29c26d03afa5f
Emiton <emiton_alves@my.uri.edu>
2019-10-05 01:22:03 -0400

    Created data model for stock object

---
+++ StockObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace financial_scraper
+{
+    class StockObject
+    {
+        public String Symbol { get; set; }
+        public String LastPrice { get; set; }
+        public String ValueChange { get; set; }
+        public String PercentChange { get; set; }
+        public String Currency { get; set; }
+        public String MarketTime { get; set; }
+        public String Volume { get; set; }
+        public String AverageVolume3M { get; set; }
+        public String MarketCap { get; set; }
+        public String ScrapeTime { get; set; }
+    }
+}
