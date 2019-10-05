ac8526d17c6b2505164450245a3af1aeb13a7392
Emiton <emiton_alves@my.uri.edu>
2019-10-05 01:28:03 -0400

    Determine algorithm for scraping process

--- Program.cs
+++ Program.cs
@@ -8,7 +8,14 @@ namespace financial_scraper
         {
             Console.WriteLine("Hello World!");
             WebScraper scraper = new WebScraper();
+            // TODO: Scraper should return table of stocks, change method name?
             scraper.ScrapePortfolio();
+            // TODOs
+                // Create list of stocks
+                // Add to DB
+                // Query DB
+                // Print all stocks out
+                // Think about DateTime for DB, what about making certain queries to DB?
         }
     }
 }
