b50740a831e40d40b3ed1852309b82d30273570e
Emiton <emiton_alves@my.uri.edu>
2019-10-06 13:15:45 -0400

    Added DB connection

---
+++ StockContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace financial_scraper
+{
+    class StockContext : DbContext
+    {
+        public StockContext(): base("StockDatabase") 
+        {
+        }
+        public DbSet<StockObject> StockObjects { get; set; }
+    }
+}
